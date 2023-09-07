1989e03e3b02f5224e74c05570b6bd800c5b3f7d
5HT <maxim@synrc.com>
2023-09-08 02:53:06 +0300

    single dependency

--- Package.swift
+++ Package.swift
@@ -7,20 +7,9 @@ let package = Package(
     name: "chat-asn1",
     platforms: [ .macOS(.v12), .iOS(.v13) ],
     products: [ .library(name: "chat-asn1", targets: ["ASN1SCG"]), ],
-    targets: [
-      .target(
-         name: "ASN1SCG",
-         dependencies: [
-            .product(name: "Crypto", package: "swift-crypto"),
-            .product(name: "_CryptoExtras", package: "swift-crypto"),
-            .product(name: "SwiftASN1", package: "swift-asn1"),
-         ]),
-    ]
+    targets: [ .target(name: "ASN1SCG", dependencies: [ .product(name: "SwiftASN1", package: "swift-asn1"), ]), ]
 )
 
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
-    package.dependencies += [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.6.0"),
-        .package(url: "https://github.com/apple/swift-asn1.git", from: "0.8.0"),
-    ]
+    package.dependencies += [ .package(url: "https://github.com/apple/swift-asn1.git", from: "0.8.0"), ]
 }
