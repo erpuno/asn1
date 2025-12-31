4a90f78622bc6968234b780dd21825de8124b2dd
5HT <maxim@synrc.com>
2025-12-31 18:35:05 +0200

    buildscripts

--- verify_java.sh
+++ verify_java.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-#sh clean.sh
-#sh rebuild_java.sh
+sh clean.sh
+sh rebuild_java.sh
 
 echo "Running"
-cd Languages/Java
-#git clone git@github.com:chat-x509/der.java der-java
+cd Languages
+git clone git@github.com:chat-x509/der.java der-java
+cd Java
 gradle run
