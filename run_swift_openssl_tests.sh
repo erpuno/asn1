5928c79abb37f06825e1509d7fb4714a8e15017a
iho <iho@users.noreply.github.com>
2025-12-31 21:24:58 +0200

    Always use DER instead defailt PEM

--- run_swift_openssl_tests.sh
+++ run_swift_openssl_tests.sh
@@ -19,23 +19,28 @@ distinguished_name = req_dn
 [req_dn]
 EOF
 
-    # RSA Key
+    # RSA Key (Internal PEM)
     openssl genrsa -out $TEST_DIR/rsa_key.pem 2048
+    # RSA Key (Test DER)
     openssl pkcs8 -topk8 -inform PEM -outform DER -in $TEST_DIR/rsa_key.pem -out $TEST_DIR/rsa_key.der -nocrypt
     
-    # EC Key
+    # EC Key (Internal PEM)
     openssl ecparam -name prime256v1 -genkey -noout -out $TEST_DIR/ec_key.pem
+    # EC Key (Test DER)
     openssl pkcs8 -topk8 -inform PEM -outform DER -in $TEST_DIR/ec_key.pem -out $TEST_DIR/ec_key.der -nocrypt
     
-    # CSR
-    openssl req -new -key $TEST_DIR/rsa_key.pem -out $TEST_DIR/csr.der -outform DER -subj "/CN=Test/O=Org" -config $TEST_DIR/min.cnf
+    # CSR (Internal PEM)
+    openssl req -new -key $TEST_DIR/rsa_key.pem -out $TEST_DIR/csr.pem -subj "/CN=Test/O=Org" -config $TEST_DIR/min.cnf
+    # CSR (Test DER)
+    openssl req -in $TEST_DIR/csr.pem -out $TEST_DIR/csr.der -outform DER -config $TEST_DIR/min.cnf
     
-    # CA Certificate
-    openssl req -x509 -key $TEST_DIR/rsa_key.pem -out $TEST_DIR/ca_cert.der -outform DER -days 365 -subj "/CN=CA" -set_serial 1234567890 -config $TEST_DIR/min.cnf
-    openssl x509 -inform DER -in $TEST_DIR/ca_cert.der -out $TEST_DIR/ca_cert.pem
+    # CA Certificate (Internal PEM)
+    openssl req -x509 -new -key $TEST_DIR/rsa_key.pem -out $TEST_DIR/ca_cert.pem -days 365 -subj "/CN=CA" -set_serial 1234567890 -config $TEST_DIR/min.cnf
+    # CA Certificate (Test DER)
+    openssl x509 -in $TEST_DIR/ca_cert.pem -out $TEST_DIR/ca_cert.der -outform DER
 
-    # EE Certificate
-    openssl x509 -req -inform DER -in $TEST_DIR/csr.der -CA $TEST_DIR/ca_cert.pem -CAkey $TEST_DIR/rsa_key.pem \
+    # EE Certificate (Internal PEM -> Test DER)
+    openssl x509 -req -in $TEST_DIR/csr.pem -CA $TEST_DIR/ca_cert.pem -CAkey $TEST_DIR/rsa_key.pem \
         -out $TEST_DIR/ee_cert.der -outform DER -days 30 -CAcreateserial
 
     # Extended cert with SAN
